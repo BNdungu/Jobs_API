fb7008a409cabb27086cfc48ea530fa82b0a384a
BNdungu <bryannganga909@gmail.com>
2024-01-28 07:02:11 +0300

    user schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const users = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name']
+    },
+
+    email: {
+        type: String,
+        required: [true, 'PLease provide your email'],
+        unique: true,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email']
+    },
+
+    paswword: {
+        type: String,
+        required: [true, 'Please provide your password']
+
+    }
+})
+
+module.exports = new mongoose.model('Users', users)
