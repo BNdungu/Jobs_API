17002ea6e486ea6ce81a6bd5d21d288f4268ad26
BNdungu <bryannganga909@gmail.com>
2024-01-30 21:34:22 +0300

    jobs model

---
+++ models/Job.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const jobsSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide a company name'],
+        maxlength: 50
+    },
+
+    position: {
+        type: String,
+        required: [true, 'Please provide the job position'],
+        maxlength: 50
+    },
+
+    status: {
+        type: String,
+        enum: ['Interveiw', 'Declined', 'Pending'],
+        default: 'Pending'
+    },
+
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: Users,
+        required: [true, 'Please provide the user']
+    }
+
+})
+
+module.exports = mongoose.model('Jobs', jobsSchema)
