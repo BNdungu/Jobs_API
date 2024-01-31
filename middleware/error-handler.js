5ae4d2c16a7f1b08d9c997b7e83bf8187a618142
BNdungu <bryannganga909@gmail.com>
2024-01-31 22:25:19 +0300

    duplicate error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,9 +6,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode : err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong please try again later'
   }
-  
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
+  }
+
+  if (err.code && err.code == 11000){
+    customError.msg = `Duplicate value for fieled ${Object.keys(err.keyValue)} entered please use another value` 
+    customError.statusCode = StatusCodes.BAD_REQUEST
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.msg })
